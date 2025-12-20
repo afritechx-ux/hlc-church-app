6cf8f7d242a8e5648a3b90824aa9c20b9d9606f5
Gabriel Kusi <kusigabriel132@gmail.com>
2025-12-20 23:09:34 +0000

    debug: add minimal public test page

---
+++ admin-web/src/app/public/test/page.tsx
@@ -0,0 +1,9 @@
+export default function PublicTestPage() {
+    return (
+        <div style={{ padding: '50px', textAlign: 'center' }}>
+            <h1>Public Test Page</h1>
+            <p>If you can see this, public routes work!</p>
+            <p>No JavaScript, no auth, no dependencies.</p>
+        </div>
+    );
+}
